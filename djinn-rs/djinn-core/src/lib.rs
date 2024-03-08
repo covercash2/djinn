129d208cb39d77e7fc25fbb33d3095d0d70e318c
Chris Overcash <covercash2@gmail.com>
2024-03-08 09:32:58 -0600

    refactor: cleanup code in djinn-core::lib

--- djinn-rs/djinn-core/src/lib.rs
+++ djinn-rs/djinn-core/src/lib.rs
@@ -1,8 +1,10 @@
 extern crate accelerate_src;
 
-use std::{net::{SocketAddr, SocketAddrV4}, sync::Arc};
+use std::{
+    net::{SocketAddr, SocketAddrV4},
+    sync::Arc,
+};
 
-use clap::{Parser, Subcommand};
 use server::{Config, Context, HttpServer};
 
 mod coco_classes;
@@ -18,40 +20,6 @@ mod token_output_stream;
 mod util;
 pub mod yolov8;
 
-#[derive(Parser)]
-struct Cli {
-    #[command(subcommand)]
-    runner: Runner,
-}
-
-#[derive(Subcommand)]
-enum Runner {
-    Server,
-    SingleRun(Args),
-}
-
-#[derive(Parser)]
-struct Args {
-    /// Run on CPU rather than on GPU.
-    #[deprecated(note = "this should be removed and moved to the specific model config")]
-    #[arg(long)]
-    pub cpu: bool,
-    /// Enable tracing (generates a trace-timestamp.json file).
-    #[arg(long)]
-    pub tracing: bool,
-    /// The model architecture used
-    #[command(subcommand)]
-    architecture: Architecture,
-}
-
-#[derive(Subcommand)]
-enum Architecture {
-    Yolov8(yolov8::args::Args),
-    Llama(llama::Args),
-    Mistral(mistral::Args),
-}
-
-// TODO re-enable server
 pub async fn run_server() -> anyhow::Result<()> {
     let addr: SocketAddrV4 = "127.0.0.1:8090".parse()?;
     let context = Context::new(Config::new(SocketAddr::from(addr)));
