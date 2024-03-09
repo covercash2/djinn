ff897a33b02904512744f4236656ca6611a3c036
Chris Overcash <covercash2@gmail.com>
2024-03-08 20:37:55 -0600

    refactor: create djinn-server package

--- djinn-rs/djinn-core/src/lib.rs
+++ djinn-rs/djinn-core/src/lib.rs
@@ -1,12 +1,5 @@
 extern crate accelerate_src;
 
-use std::{
-    net::{SocketAddr, SocketAddrV4},
-    sync::Arc,
-};
-
-use server::{Config, Context, HttpServer};
-
 mod coco_classes;
 pub mod device;
 mod error;
@@ -14,16 +7,7 @@ mod font;
 pub mod llama;
 pub mod lm;
 pub mod mistral;
-mod server;
 mod text_generator;
 mod token_output_stream;
 mod util;
 pub mod yolov8;
-
-pub async fn run_server() -> anyhow::Result<()> {
-    let addr: SocketAddrV4 = "127.0.0.1:8090".parse()?;
-    let context = Context::new(Config::new(SocketAddr::from(addr)));
-    let server = HttpServer::new(Arc::from(context));
-
-    server.start().await
-}
