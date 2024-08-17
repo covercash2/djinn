23dc8c3b57b8ed5f213f243a4f13c36d0f00bb8a
Chris Overcash <covercash2@gmail.com>
2024-08-17 14:39:47 -0500

    chore: refactor model code and add Starcoder

--- djinn-rs/djinn-core/src/lm/mod.rs
+++ djinn-rs/djinn-core/src/lm/mod.rs
@@ -1,37 +1,38 @@
 //! Language Models and configurations
-use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
 use futures_util::Stream;
+use serde::{Deserialize, Serialize};
 
 use crate::error::Result;
 
+use self::mistral::RunConfig;
+
 pub mod mistral;
-
-// #[derive(Parser)]
-// pub struct Args {
-//     prompt: String,
-//     /// The seed to use when generating random samples.
-//     #[arg(long, default_value_t = 299792458)]
-//     seed: u64,
-//     /// The length of the sample to generate (in tokens).
-//     #[arg(long, short = 'n', default_value_t = 100)]
-//     sample_len: usize,
-//
-//     #[command(subcommand)]
-//     model: LanguageModel,
-// }
-
-/// Language models
-// #[derive(Subcommand)]
-// pub enum LanguageModel {
-//     Mistral(mistral::Args),
-// }
+pub mod model;
 
 pub trait Lm {
-    type Config;
+    // type Config;
+    type Weights;
 
     fn run(
         &mut self,
         prompt: String,
-        config: Self::Config,
+        config: RunConfig,
+        model: Self::Weights,
     ) -> impl Stream<Item = Result<String>> + '_;
+}
+
+/// Where to load the model from,
+/// either HuggingFaceHub or from the file system
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum ModelSource {
+    HuggingFaceHub {
+        revision: String,
+    },
+    Files {
+        weight_files: Vec<PathBuf>,
+        tokenizer_file: PathBuf,
+    },
 }
