0f765bada63847899d60fead09a98951449ce31a
Chris Overcash <covercash2@gmail.com>
2024-08-17 16:04:04 -0500

    fix: remove HTMX

--- djinn-rs/djinn-server/src/handlers/complete.rs
+++ djinn-rs/djinn-server/src/handlers/complete.rs
@@ -1,10 +1,9 @@
 use std::ops::DerefMut;
 use std::sync::Arc;
 
-use askama::Template;
+use axum::debug_handler;
 use axum::extract::State;
 use axum::response::Html;
-use axum::Form;
 use djinn_core::lm::mistral::RunConfig;
 use djinn_core::lm::Lm;
 use futures_util::{pin_mut, StreamExt};
@@ -24,8 +23,6 @@ pub struct CompleteRequest {
     config: RunConfig,
 }
 
-#[derive(Template)]
-#[template(path = "response.html")]
 #[derive(Serialize, Deserialize, Debug)]
 pub struct CompleteResponse {
     prompt: String,
@@ -47,25 +44,6 @@ pub async fn complete(
     let response = run_model(context, payload).await?;
 
     Ok(Json(response))
-}
-
-#[instrument(skip(model_context))]
-pub async fn complete_form(
-    State(model_context): State<Arc<Mutex<Context>>>,
-    Form(payload): Form<CompleteRequest>,
-) -> Result<Html<String>> {
-    let span = tracing::info_span!("complete form get lock");
-
-    let mut lock = model_context.lock().instrument(span).await;
-    tracing::info!("got model lock");
-
-    let context: &mut Context = lock.deref_mut();
-
-    let response = run_model(context, payload)
-        .await
-        .map(|response| markdown::to_html(&response.output))?;
-
-    Ok(Html(response))
 }
 
 #[instrument(skip(model_context))]
