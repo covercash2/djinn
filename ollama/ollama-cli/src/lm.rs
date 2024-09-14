1c0e485aac1f1f9253c5544c04c6458586dc1fd2
Chris Overcash <covercash2@gmail.com>
2024-09-14 13:45:02 -0500

    feat: add chat functions

--- ollama/ollama-cli/src/lm.rs
+++ ollama/ollama-cli/src/lm.rs
@@ -1,7 +1,14 @@
 use std::sync::Arc;
 
+use crate::ollama::chat::ChatRequest;
+
 pub enum Response {
     Eos,
     Error(Arc<str>),
     Token(Arc<str>),
+}
+
+pub enum Prompt {
+    Generate(Arc<str>),
+    Chat(ChatRequest),
 }
