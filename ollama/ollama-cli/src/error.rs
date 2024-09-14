e89df7e44666ea02e54e771cef14d072ea87a081
Chris Overcash <covercash2@gmail.com>
2024-09-13 19:26:43 -0500

    feat: add async streaming to the TUI

---
+++ ollama/ollama-cli/src/error.rs
@@ -0,0 +1,16 @@
+use ollama_rs::error::OllamaError;
+use thiserror::Error;
+use tokio::sync::mpsc::error::SendError;
+
+use crate::lm::Response;
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    OllamaRs(#[from] OllamaError),
+
+    #[error("error sending Response over channel")]
+    SendResponse(#[from] SendError<Response>),
+}
