243a4eb5c49105292a3980c9dfa2063acd07405d
Chris Overcash <covercash2@gmail.com>
2024-10-12 14:13:02 -0500

    chore: add human readable sizes

--- ollama/ollama-cli/src/tui/models/mod.rs
+++ ollama/ollama-cli/src/tui/models/mod.rs
@@ -48,6 +48,18 @@ impl ModelsViewModel {
         match action {
             Action::Refresh => Ok(Some(AppEvent::Submit(Prompt::LocalModels))),
             Action::Quit => Ok(Some(AppEvent::Quit)),
+            Action::Enter => {
+                self.list_state.select(Some(0));
+                Ok(None)
+            }
+            Action::Down => {
+                self.list_state.select_next();
+                Ok(None)
+            }
+            Action::Up => {
+                self.list_state.select_previous();
+                Ok(None)
+            }
             _ => Ok(None),
         }
     }
@@ -70,13 +82,56 @@ pub impl<'a> Frame<'a> {
             .iter()
             .map(|info| {
                 let name = Span::from(info.name.as_str());
-                let size = Span::from(info.size.to_string());
+                let size = Span::from(info.size.fit_to_bytesize());
                 ListItem::from(Line::from_iter([name, size]))
             })
             .collect();
 
-        let list = models.block(Block::bordered()).style(style);
+        let list = models
+            .block(Block::bordered())
+            .style(style)
+            .highlight_symbol(">>");
 
         self.render_stateful_widget(list, model_info_area, &mut view_model.list_state);
+    }
+}
+
+const BYTE_UNITS: &[&str] = &["B", "KiB", "MiB", "GiB", "TiB", "PiB"];
+
+#[extend::ext]
+pub impl u64 {
+    fn to_kib(&self) -> String {
+        let value = self.div_ceil(1024);
+        format!("{value}KiB")
+    }
+
+    fn to_mib(&self) -> String {
+        let value = self.div_ceil(1024 ^ 2);
+        format!("{value}MiB")
+    }
+
+    fn to_gib(&self) -> String {
+        let value = self.div_ceil(1024 ^ 3);
+        format!("{value}GiB")
+    }
+
+    fn fit_to_bytesize(&self) -> String {
+        let (value, order) = std::iter::repeat(())
+            .enumerate()
+            .map(|(i, ())| i)
+            .scan(*self, |state, i| {
+                if *state >= 1024 {
+                    *state /= 1024;
+                    Some((*state, i + 1))
+                } else {
+                    None
+                }
+            })
+            .last()
+            .unwrap_or((*self, 0));
+
+        let unit = BYTE_UNITS[order];
+
+        format!("{value}{unit}")
     }
 }
