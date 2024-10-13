830b209c56bac8e2611b52b34955f30e78b8847c
Chris Overcash <covercash2@gmail.com>
2024-10-13 13:24:01 -0500

    feat: add ability to open editor

--- ollama/ollama-cli/src/tui/models/model_info.rs
+++ ollama/ollama-cli/src/tui/models/model_info.rs
@@ -1,11 +1,24 @@
 use ollama_rs::models::ModelInfo;
-use ratatui::{layout::Rect, style::Style, text::Text, widgets::{Block, Paragraph}, Frame};
+use ratatui::{
+    layout::Rect,
+    style::Style,
+    text::Text,
+    widgets::{Block, Paragraph, Wrap},
+    Frame,
+};
 
-use crate::{error::{Error, Result }, lm::Response};
+use crate::{
+    error::{Error, Result},
+    lm::Response,
+    tui::event::Action,
+};
+
+use super::ModelEvent;
 
 #[derive(Debug, Clone, Default)]
 pub struct ModelInfoViewModel {
     info: Option<ModelInfo>,
+    scroll_offset: u16,
 }
 
 impl ModelInfoViewModel {
@@ -15,6 +28,34 @@ impl ModelInfoViewModel {
             Ok(())
         } else {
             Err(Error::UnexpectedResponse(response))
+        }
+    }
+
+    pub fn handle_action(&mut self, action: Action) -> Result<Option<ModelEvent>> {
+        match action {
+            Action::Up => {
+                self.scroll_offset = self.scroll_offset.saturating_sub(1);
+                Ok(None)
+            }
+            Action::Down => {
+                self.scroll_offset = self.scroll_offset.saturating_add(1);
+                Ok(None)
+            }
+            Action::Refresh => Ok(Some(ModelEvent::Refresh)),
+            Action::Quit => Ok(Some(ModelEvent::Deactivate)),
+            Action::Enter => {
+                if let Some(ref model) = self.info {
+                    Ok(Some(ModelEvent::EditInfo(model.clone())))
+                } else {
+                    tracing::info!("no model selected to edit",);
+                    Ok(None)
+                }
+            }
+            Action::Left
+            | Action::Right
+            | Action::LeftWord
+            | Action::RightWord
+            | Action::Unhandled => Ok(None),
         }
     }
 }
@@ -29,10 +70,11 @@ pub impl<'a> Frame<'a> {
         };
 
         let widget = Paragraph::new(text)
+            .scroll((view_model.scroll_offset, 0))
+            .wrap(Wrap { trim: false })
             .block(Block::bordered())
             .style(style);
 
         self.render_widget(widget, parent);
     }
 }
-
