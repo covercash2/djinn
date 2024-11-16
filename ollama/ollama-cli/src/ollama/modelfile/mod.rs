367289d170779ca5e649379285acc8c590ad103b
Chris Overcash <covercash2@gmail.com>
2024-11-16 15:42:22 -0600

    feat: parse full modelfiles

--- ollama/ollama-cli/src/ollama/modelfile/mod.rs
+++ ollama/ollama-cli/src/ollama/modelfile/mod.rs
@@ -5,23 +5,195 @@
 //! to define a derived model.
 //! This is an attempt to parse that genius file format in Rust.
 
-use std::path::PathBuf;
+use std::{path::PathBuf, str::FromStr};
 
+use nom::instructions;
 use strum::{EnumDiscriminants, EnumIter, EnumString, IntoStaticStr, VariantArray};
+
+use super::chat::Message;
 
 mod nom;
 
 #[cfg(test)]
-pub mod tests;
+pub mod test_data;
 
-pub struct ModelFile {
+#[derive(Clone, Debug)]
+pub struct Modelfile {
     from: String,
     parameters: Vec<Parameter>,
     template: Option<String>,
     system: Option<String>,
-    adapters: Option<String>,
+    adapter: Option<TensorFile>,
     license: Option<String>,
-    messages: Vec<String>,
+    messages: Vec<Message>,
+}
+
+impl FromStr for Modelfile {
+    type Err = String;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let instructions: Vec<Instruction> = instructions(input)
+            .map_err(|error| error.to_string())
+            .and_then(|(rest, instructions)| {
+                if rest.is_empty() {
+                    Ok(instructions)
+                } else {
+                    Err("parser did not consume all input".into())
+                }
+            })?;
+
+        let mut modelfile = ModelfileBuilder::default();
+
+        for instruction in instructions {
+            let _ = match instruction {
+                Instruction::From(model) => modelfile.from(model)?,
+                Instruction::Parameter(parameter) => modelfile.parameter(parameter),
+                Instruction::Template(template) => modelfile.template(template)?,
+                Instruction::System(system) => modelfile.system(system)?,
+                Instruction::Adapter(tensor_file) => modelfile.adapter(tensor_file)?,
+                Instruction::License(license) => modelfile.license(license),
+                Instruction::Message(message) => modelfile.message(message),
+                Instruction::Skip => &mut modelfile,
+            };
+        }
+
+        modelfile.build()
+    }
+}
+
+#[derive(Clone, Debug, Default)]
+pub struct ModelfileBuilder {
+    from: Option<String>,
+    parameters: Vec<Parameter>,
+    template: Option<String>,
+    system: Option<String>,
+    adapter: Option<TensorFile>,
+    license: Option<String>,
+    messages: Vec<Message>,
+}
+
+impl ModelfileBuilder {
+    pub fn build(self) -> Result<Modelfile, String> {
+        let ModelfileBuilder {
+            from,
+            parameters,
+            template,
+            system,
+            adapter,
+            license,
+            messages,
+        } = self;
+        if let Some(from) = from {
+            Ok(Modelfile {
+                from,
+                parameters,
+                template,
+                system,
+                adapter,
+                license,
+                messages,
+            })
+        } else {
+            Err("Modelfile requires a FROM instruction".into())
+        }
+    }
+
+    pub fn from(&mut self, input: String) -> Result<&mut Self, String> {
+        if self.from.is_some() {
+            Err(format!(
+                "Modelfile can only have one FROM instruction: {}",
+                input
+            ))
+        } else {
+            self.from = Some(input);
+            Ok(self)
+        }
+    }
+
+    pub fn parameter(&mut self, parameter: Parameter) -> &mut Self {
+        self.parameters.push(parameter);
+        self
+    }
+
+    pub fn template(&mut self, template: String) -> Result<&mut Self, String> {
+        if self.template.is_some() {
+            Err(format!(
+                "Modelfile can only have one TEMPLATE instruction: {}",
+                template
+            ))
+        } else {
+            self.template = Some(template);
+            Ok(self)
+        }
+    }
+
+    pub fn system(&mut self, system: String) -> Result<&mut Self, String> {
+        if self.system.is_some() {
+            Err(format!(
+                "Modelfile can only have one SYSTEM instruction: {}",
+                system,
+            ))
+        } else {
+            self.system = Some(system);
+            Ok(self)
+        }
+    }
+
+    pub fn adapter(&mut self, adapter: TensorFile) -> Result<&mut Self, String> {
+        if self.adapter.is_some() {
+            Err(format!(
+                "Modelfile can only have one ADAPTER instruction: {:?}",
+                adapter,
+            ))
+        } else {
+            self.adapter = Some(adapter);
+            Ok(self)
+        }
+    }
+
+    pub fn license(&mut self, license: String) -> &mut Self {
+        if let Some(existing) = &self.license {
+            self.license = Some(format!("{existing}\n{license}"));
+        } else {
+            self.license = Some(license);
+        }
+
+        self
+    }
+
+    pub fn message(&mut self, message: Message) -> &mut Self {
+        self.messages.push(message);
+        self
+    }
+}
+
+pub enum Instruction {
+    Skip,
+    From(String),
+    Parameter(Parameter),
+    Template(String),
+    System(String),
+    Adapter(TensorFile),
+    License(String),
+    Message(Message),
+}
+
+impl From<Parameter> for Instruction {
+    fn from(value: Parameter) -> Self {
+        Instruction::Parameter(value)
+    }
+}
+
+impl From<TensorFile> for Instruction {
+    fn from(value: TensorFile) -> Self {
+        Instruction::Adapter(value)
+    }
+}
+
+impl From<Message> for Instruction {
+    fn from(value: Message) -> Self {
+        Instruction::Message(value)
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -103,4 +275,25 @@ pub enum Parameter {
     /// logits with a value less than 0.045 are filtered out.
     /// (Default: 0.0)
     MinP(f32),
+}
+
+#[cfg(test)]
+mod tests {
+    use test_data::{load_modelfiles, TEST_DATA_DIR};
+
+    use super::*;
+
+    #[test]
+    fn modelfiles_are_parsed() {
+        let modelfiles: Vec<(PathBuf, String)> = load_modelfiles(TEST_DATA_DIR);
+
+        for (path, case) in modelfiles {
+            dbg!(&path);
+            let modelfile: Modelfile = case
+                .parse::<Modelfile>()
+                .expect("should be able to parse Modelfile");
+
+            dbg!(modelfile);
+        }
+    }
 }
--- ollama/ollama-cli/src/ollama/modelfile/nom.rs
+++ ollama/ollama-cli/src/ollama/modelfile/nom.rs
@@ -21,30 +21,54 @@ use nom::{
         complete::tag_no_case,
         streaming::{tag, take_until, take_while, take_while1},
     },
-    character::complete::{self, multispace1, newline},
+    character::complete::{self, multispace0, multispace1},
     combinator::{eof, value},
-    error::context,
-    multi::many0,
+    error::{context, dbg_dmp},
+    multi::{many0, many1, many_till},
     sequence::{delimited, pair, preceded, terminated},
     IResult, Parser as _,
 };
 
 use crate::ollama::chat::{Message, MessageRole};
 
-use super::{Parameter, ParameterName, TensorFile};
+use super::{Instruction, Parameter, ParameterName, TensorFile};
 
 const TRIPLE_QUOTES: &str = r#"""""#;
 const SINGLE_QUOTE: &str = r#"""#;
-const SINGLE_QUOTE_NEWLINE: &str = r#""\n"#;
+
+pub fn instructions(input: &str) -> IResult<&str, Vec<Instruction>> {
+    many_till(instruction, eof)
+        .map(|(instruction, _eof)| instruction)
+        .parse(input)
+}
+
+pub fn instruction(input: &str) -> IResult<&str, Instruction> {
+    context(
+        "instruction",
+        alt((
+            skip_lines,
+            from,
+            parameter_line,
+            template,
+            system,
+            adapter,
+            license,
+            message,
+        )),
+    )
+    .parse(input)
+}
 
 /// Takes an input string and returns a `ModelName`.
 /// Parses a line that starts with `FROM`
 /// that specifies the [`ModelId`]
-pub fn from(input: &str) -> IResult<&str, &str> {
+pub fn from(input: &str) -> IResult<&str, Instruction> {
     let from_tag = tag_no_case("FROM");
     let space = take_while1(|c| c == ' ');
 
-    context("FROM", preceded(pair(from_tag, space), model_id)).parse(input)
+    context("FROM", preceded(pair(from_tag, space), model_id))
+        .map(|model| Instruction::From(model.to_string()))
+        .parse(input)
 }
 
 pub fn model_id(input: &str) -> IResult<&str, &str> {
@@ -71,8 +95,13 @@ pub fn comment(input: &str) -> IResult<&str, ()> {
 }
 
 /// Consume empty lines and comments
-pub fn skip_lines(input: &str) -> IResult<&str, ()> {
-    context("skip_lines", value((), many0(comment))).parse(input)
+pub fn skip_lines(input: &str) -> IResult<&str, Instruction> {
+    let skip = alt((value((), many1(comment)), value((), multispace1)));
+    // let skip = many1(comment);
+
+    context("skip_lines", value((), skip))
+        .map(|()| Instruction::Skip)
+        .parse(input)
 }
 
 /// Parse a model template.
@@ -90,13 +119,18 @@ pub fn skip_lines(input: &str) -> IResult<&str, ()> {
 /// Leave that to the geniuses at Ollama.
 ///
 /// [the spec]: https://github.com/ollama/ollama/blob/main/docs/modelfile.md#template
-fn template(input: &str) -> IResult<&str, &str> {
+fn template(input: &str) -> IResult<&str, Instruction> {
     let template = tag_no_case("TEMPLATE");
     let space = take_while(|c| c == ' ');
-    preceded(
-        pair(template, space),
-        alt((triple_quote_string, single_quoted_multiline_string)),
+    context(
+        "TEMPLATE",
+        preceded(
+            pair(template, space),
+            // alt((triple_quote_string, single_quoted_multiline_string)),
+            multiline,
+        ),
     )
+    .map(|template| Instruction::Template(template.into()))
     .parse(input)
 }
 
@@ -206,8 +240,8 @@ pub fn int_parameter_value(input: &str) -> IResult<&str, usize> {
     })
 }
 
-pub fn string_parameter_value(input: &str) -> IResult<&str, &str> {
-    complete::not_line_ending(input)
+pub fn string_parameter_value(input: &str) -> IResult<&str, String> {
+    complete::not_line_ending.map(Into::into).parse(input)
 }
 
 pub fn parameter(input: &str) -> IResult<&str, Parameter> {
@@ -215,46 +249,28 @@ pub fn parameter(input: &str) -> IResult<&str, Parameter> {
     let (input, name) = parameter_name(input)?;
     dbg!(name);
     match name {
-        ParameterName::Mirostat => {
-            int_parameter_value(input).map(|(rest, int)| (rest, Parameter::Mirostat(int)))
-        }
-        ParameterName::MirostatEta => {
-            float_parameter_value(input).map(|(rest, value)| (rest, Parameter::MirostatEta(value)))
-        }
-        ParameterName::MirostatTau => {
-            float_parameter_value(input).map(|(rest, value)| (rest, Parameter::MirostatTau(value)))
-        }
-        ParameterName::NumCtx => {
-            int_parameter_value(input).map(|(rest, value)| (rest, Parameter::NumCtx(value)))
-        }
-        ParameterName::RepeatLastN => {
-            int_parameter_value(input).map(|(rest, value)| (rest, Parameter::RepeatLastN(value)))
-        }
-        ParameterName::RepeatPenalty => float_parameter_value(input)
-            .map(|(rest, value)| (rest, Parameter::RepeatPenalty(value))),
-        ParameterName::Temperature => {
-            float_parameter_value(input).map(|(rest, value)| (rest, Parameter::Temperature(value)))
-        }
-        ParameterName::Seed => {
-            int_parameter_value(input).map(|(rest, value)| (rest, Parameter::Seed(value)))
-        }
-        ParameterName::Stop => string_parameter_value(input)
-            .map(|(rest, value)| (rest, Parameter::Stop(value.to_string()))),
-        ParameterName::TfsZ => {
-            float_parameter_value(input).map(|(rest, value)| (rest, Parameter::TfsZ(value)))
-        }
-        ParameterName::NumPredict => {
-            int_parameter_value(input).map(|(rest, value)| (rest, Parameter::NumPredict(value)))
-        }
-        ParameterName::TopK => {
-            int_parameter_value(input).map(|(rest, value)| (rest, Parameter::TopK(value)))
-        }
-        ParameterName::TopP => {
-            float_parameter_value(input).map(|(rest, value)| (rest, Parameter::TopP(value)))
-        }
-        ParameterName::MinP => {
-            float_parameter_value(input).map(|(rest, value)| (rest, Parameter::MinP(value)))
-        }
+        ParameterName::Mirostat => int_parameter_value.map(Parameter::Mirostat).parse(input),
+        ParameterName::MirostatEta => float_parameter_value
+            .map(Parameter::MirostatEta)
+            .parse(input),
+        ParameterName::MirostatTau => float_parameter_value
+            .map(Parameter::MirostatTau)
+            .parse(input),
+        ParameterName::NumCtx => int_parameter_value.map(Parameter::NumCtx).parse(input),
+        ParameterName::RepeatLastN => int_parameter_value.map(Parameter::RepeatLastN).parse(input),
+        ParameterName::RepeatPenalty => float_parameter_value
+            .map(Parameter::RepeatPenalty)
+            .parse(input),
+        ParameterName::Temperature => float_parameter_value
+            .map(Parameter::Temperature)
+            .parse(input),
+        ParameterName::Seed => int_parameter_value.map(Parameter::Seed).parse(input),
+        ParameterName::Stop => string_parameter_value.map(Parameter::Stop).parse(input),
+        ParameterName::TfsZ => float_parameter_value.map(Parameter::TfsZ).parse(input),
+        ParameterName::NumPredict => int_parameter_value.map(Parameter::NumPredict).parse(input),
+        ParameterName::TopK => int_parameter_value.map(Parameter::TopK).parse(input),
+        ParameterName::TopP => float_parameter_value.map(Parameter::TopP).parse(input),
+        ParameterName::MinP => float_parameter_value.map(Parameter::MinP).parse(input),
     }
 }
 
@@ -263,47 +279,61 @@ pub fn parameter(input: &str) -> IResult<&str, Parameter> {
 /// to an arbitrary string value
 ///
 /// https://github.com/ollama/ollama/blob/main/docs/modelfile.md#parameter
-pub fn parameter_line(input: &str) -> IResult<&str, Parameter> {
+pub fn parameter_line(input: &str) -> IResult<&str, Instruction> {
     let parameter_tag = tag_no_case("PARAMETER");
 
     dbg!(&input);
 
-    preceded(pair(parameter_tag, multispace1), parameter).parse(input)
+    context(
+        "PARAMETER",
+        preceded(pair(parameter_tag, multispace1), parameter).map(Into::into),
+    )
+    .parse(input)
 }
 
 pub fn multiline(input: &str) -> IResult<&str, &str> {
-    alt((
-        triple_quote_string,
-        single_quoted_multiline_string,
-        complete::not_line_ending,
-    ))
+    context(
+        "multiline",
+        alt((
+            triple_quote_string,
+            single_quoted_multiline_string,
+            complete::not_line_ending,
+        )),
+    )
     .parse(input)
 }
 
 /// The system message to be used in the template, if applicable
 ///
 /// https://github.com/ollama/ollama/blob/main/docs/modelfile.md#system
-pub fn system(input: &str) -> IResult<&str, &str> {
+pub fn system(input: &str) -> IResult<&str, Instruction> {
     let template = tag_no_case("system");
-    preceded(pair(template, multispace1), multiline).parse(input)
+    context(
+        "SYSTEM",
+        preceded(pair(template, multispace1), multiline)
+            .map(|system| Instruction::System(system.into())),
+    )
+    .parse(input)
 }
 
 /// Takes an input string and returns a `ModelName`.
 /// Parses a line that starts with `FROM`
 /// that specifies the [`ModelId`]
-pub fn adapter(input: &str) -> IResult<&str, TensorFile> {
+pub fn adapter(input: &str) -> IResult<&str, Instruction> {
     let adapter_tag = tag_no_case("adapter");
 
     context(
         "ADAPTER",
         preceded(pair(adapter_tag, multispace1), tensor_file),
     )
+    .map(Into::into)
     .parse(input)
 }
 
 pub fn tensor_file(input: &str) -> IResult<&str, TensorFile> {
-    filename
-        .map(|filename| {
+    context(
+        "TensorFile",
+        filename.map(|filename| {
             if filename.ends_with(".gguf") {
                 TensorFile::Gguf(PathBuf::from(filename))
             } else if filename.ends_with(".safetensors") {
@@ -311,8 +341,9 @@ pub fn tensor_file(input: &str) -> IResult<&str, TensorFile> {
             } else {
                 panic!("bad file extensions");
             }
-        })
-        .parse(input)
+        }),
+    )
+    .parse(input)
 }
 
 pub fn filename(input: &str) -> IResult<&str, &str> {
@@ -323,55 +354,63 @@ pub fn is_file_char(c: char) -> bool {
     c.is_alphanumeric() || c == '/' || c == '.' || c == '-' || c == '_'
 }
 
-pub fn license(input: &str) -> IResult<&str, &str> {
+pub fn license(input: &str) -> IResult<&str, Instruction> {
     let license_tag = tag_no_case("license");
-    preceded(pair(license_tag, multispace1), multiline)(input)
+    context(
+        "LICENSE",
+        preceded(pair(license_tag, multispace1), multiline)
+            .map(|license| Instruction::License(license.into())),
+    )
+    .parse(input)
 }
 
-pub fn message(input: &str) -> IResult<&str, Message> {
+pub fn message(input: &str) -> IResult<&str, Instruction> {
     let message_tag = tag_no_case("message");
     let user_tag = tag("user");
     let assistant_tag = tag("assistant");
     let system_tag = tag("system");
     let role = alt((user_tag, assistant_tag, system_tag));
 
-    preceded(pair(message_tag, multispace1), pair(role, multiline))
-        .map(|(role, message)| {
+    context(
+        "MESSAGE",
+        preceded(pair(message_tag, multispace1), pair(role, multiline)).map(|(role, message)| {
             let role: MessageRole = role.parse().expect("should be able to parse role from tag");
-            (role, message).into()
-        })
-        .parse(input)
+            Message::from((role, message))
+        }),
+    )
+    .map(Into::into)
+    .parse(input)
 }
 
 #[cfg(test)]
 mod tests {
-    use std::path::Path;
-
     use strum::IntoEnumIterator;
 
-    use crate::ollama::modelfile::tests::{TEST_COMMENTS, TEST_FROM, TEST_MODEL_IDS, TEST_SINGLE_QUOTE_MULTILINE, TEST_TRIPLE_QUOTES};
+    use crate::ollama::modelfile::test_data::{
+        load_modelfiles, TEST_COMMENTS, TEST_DATA_DIR, TEST_FROM, TEST_MODEL_IDS,
+        TEST_SINGLE_QUOTE_MULTILINE, TEST_TRIPLE_QUOTES,
+    };
 
     use super::*;
 
-    const TEST_DATA_DIR: &str = "./test";
+    #[test]
+    fn instructions_are_parsed() {
+        let modelfiles: Vec<(PathBuf, String)> = load_modelfiles(TEST_DATA_DIR);
 
-    fn load_modelfiles(test_dir: impl AsRef<Path>) -> Vec<String> {
-        test_dir
-            .as_ref()
-            .read_dir()
-            .expect("could not open test data dir")
-            .map(|file| {
-                let file = file.expect("error reading dir entry");
-                file.path()
-            })
-            .filter(|path| path.ends_with("Modelfile"))
-            .map(|path| std::fs::read_to_string(path).expect("could not read file contents"))
-            .collect()
+        for (path, case) in modelfiles {
+            dbg!(&path);
+            instructions(&case).expect("should be able to parse instructions from Modelfile");
+        }
     }
 
     #[test]
-    fn test_data_loads() {
-        let _modelfiles: Vec<String> = load_modelfiles(TEST_DATA_DIR);
+    fn single_instruction_is_parsed() {
+        for case in TEST_FROM {
+            dbg!(&case);
+            instruction(case).expect("should be able to parse single instruction");
+            instructions(case)
+                .expect("should be able to parse single instruction with `instructions`");
+        }
     }
 
     #[test]
