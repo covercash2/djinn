df994811f2097dff4763960dec76298b87e5b3e4
Chris Overcash <covercash2@gmail.com>
2024-11-25 20:00:16 -0600

    chore: cargo fmt

--- ollama-cli/src/fs_ext.rs
+++ ollama-cli/src/fs_ext.rs
@@ -3,11 +3,8 @@ use std::path::Path;
 use crate::error::Result;
 
 pub fn read_file_to_string(path: impl AsRef<Path>) -> Result<String> {
-    std::fs::read_to_string(&path)
-        .map_err(|source| {
-            crate::error::Error::ReadFile {
-                source,
-                path: path.as_ref().into()
-            }
-        })
+    std::fs::read_to_string(&path).map_err(|source| crate::error::Error::ReadFile {
+        source,
+        path: path.as_ref().into(),
+    })
 }
