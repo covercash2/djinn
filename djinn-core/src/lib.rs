81aac5ebabba2efc407df80efa2202f3b9bf8c24
Chris Overcash <covercash2@gmail.com>
2025-11-30 19:52:27 -0600

    chore: add StableDiffussion from candle examples

--- djinn-core/src/lib.rs
+++ djinn-core/src/lib.rs
@@ -2,13 +2,14 @@
 extern crate accelerate_src;
 
 mod coco_classes;
-pub mod config;
-pub mod device;
 mod error;
 mod font;
 mod hf_hub_ext;
-pub mod lm;
 mod token_output_stream;
+pub mod config;
+pub mod device;
+pub mod image;
+pub mod lm;
 pub mod yolov8;
 
 pub use error::Error;
